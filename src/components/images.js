6603bc346640e957c16481c52f9802111110e65a
Logan Negley <logannegley@gmail.com>
2019-11-19 21:57:49 -0600

    brought in data from api

--- src/components/images.js
+++ src/components/images.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import CardOfTheDay from "./CardOfTheDay";
 
 
 
 
 
 function Images() {
+
+  const [photoData, setPhotoData] = useState(0); 
+
+useEffect(() => {
+axios.get("https://api.nasa.gov/planetary/apod?api_key=VhCUAbfQp5bdWSSgDKedirH0JYa0Bfu8kxyyFVmk")
+.then(response => {
+  console.log(response);
+  setPhotoData(response.data)
+  console.log(photoData)
+})
+},[]);
+
+
+
+
     return (
-      
-      
+      <div className= "container">
+
+      </div>
+
     );
   }
   
-  export default App;
+  export default Images;
   
